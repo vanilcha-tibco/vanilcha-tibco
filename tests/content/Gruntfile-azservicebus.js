8cbe7d6b60cef059a39c37405a77459ca45d104c
Neha Sinha <nsinha@tibco.com>
2018-10-22 14:29:55 -0700

    Adding gruntfile

---
+++ tests/content/Gruntfile-azservicebus.js
@@ -0,0 +1,35 @@
+module.exports = function (grunt) {
+
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        protractor: {
+
+            connector_azservicebus_smoke: {
+                options: {
+                    keepAlive: false,
+                    configFile: "./WI/config/protractorwiConf.js",
+                    args: {
+                        specs: ['./connectors/azservicebus/content/tests/wi_azservicebus.js']
+                    }
+                }
+            },
+
+            connector_azservicebus_uat: {
+                options: {
+                    keepAlive: false,
+                    configFile: "./WI/config/protractorwiConf.js",
+                    args: {
+                        specs: ['./connectors/azservicebus/content/tests/wi_azservicebus.js']
+                    }
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('test:connector_azservicebus_smoke', ['protractor:connector_azservicebus_smoke']);
+    grunt.registerTask('test:connector_azservicebus_uat', ['protractor:connector_azservicebus_uat']);
+
+};
