a6538564c694d3e23ac79cc26d42821fdf958643
Balaji Venkatamohan <bvenkata@tibco.com>
2019-04-16 19:19:13 +0530

    https://jira.tibco.com/browse/WIAZSB-76 - Adding support for sessions in Queue Receiver

--- src/app/AzureServiceBus/trigger/queuereceiver/trigger.go
+++ src/app/AzureServiceBus/trigger/queuereceiver/trigger.go
@@ -39,15 +39,57 @@ type SBQueueReceiverTrigger struct {
 
 // QueueReceiver is structure of a single QueueReceiver
 type QueueReceiver struct {
-	handler             *trigger.Handler
-	q                   *servicebus.Queue
-	listenerHandler     *servicebus.ListenerHandle
+	handler            *trigger.Handler
+	q                  *servicebus.Queue
+	stepSessionHandler *StepSessionHandler
+	sessionID          string
+	//listenerHandler     *servicebus.ListenerHandle
 	listenctxCancelFunc context.CancelFunc
 	queueName           string
 	connString          string
 	valueType           string
 	receiveMode         string
 	done                chan bool
+}
+
+// StepSessionHandler is a comment
+type StepSessionHandler struct {
+	messageSession *servicebus.MessageSession
+	valueType      string
+	queueName      string
+	handler        *trigger.Handler
+}
+
+// Start is called when a new session is started
+func (ssh *StepSessionHandler) Start(ms *servicebus.MessageSession) error {
+	ssh.messageSession = ms
+	if ssh.messageSession.SessionID() != nil {
+		log.Infof("Begin session: ", *ssh.messageSession.SessionID())
+	} else {
+		log.Infof("Begin listening to all sessions for the queue: ", ssh.queueName)
+	}
+
+	return nil
+}
+
+// Handle is called when a new session message is received
+func (ssh *StepSessionHandler) Handle(ctx context.Context, msg *servicebus.Message) error {
+	err2 := ssh.processMessage(msg, ssh.handler, ssh.valueType, ssh.queueName)
+	if err2 == nil {
+		return msg.Complete(ctx)
+	}
+	return msg.Abandon(ctx)
+}
+
+// End is called when the message session is closed. Service Bus will not automatically end your message session. Be
+// sure to know when to terminate your own session.
+func (ssh *StepSessionHandler) End() {
+	if ssh.messageSession.SessionID() != nil {
+		log.Infof("End session: ", *ssh.messageSession.SessionID())
+	} else {
+		log.Infof("End session handler for all sessions: ")
+	}
+	log.Debugf("")
 }
 
 // Initialize QueueReceiverTrigger
@@ -63,6 +105,7 @@ func (t *SBQueueReceiverTrigger) Initialize(ctx trigger.InitContext) error {
 		queueName := handler.GetStringSetting("queue")
 		receiveMode := handler.GetStringSetting("receiveMode")
 		valueType := handler.GetStringSetting("valueType")
+		sessionID := handler.GetStringSetting("sessionId")
 		connStr := "Endpoint=sb://" + config.ResourceURI + ".servicebus.windows.net/;SharedAccessKeyName=" + config.AuthorizationRuleName + ";SharedAccessKey=" + config.PrimarysecondaryKey
 		qrcvr := &QueueReceiver{}
 		qrcvr.handler = handler
@@ -70,6 +113,7 @@ func (t *SBQueueReceiverTrigger) Initialize(ctx trigger.InitContext) error {
 		qrcvr.queueName = queueName
 		qrcvr.receiveMode = receiveMode
 		qrcvr.valueType = valueType
+		qrcvr.sessionID = sessionID
 		qrcvr.done = make(chan bool)
 		t.queueReceivers = append(t.queueReceivers, qrcvr)
 	}
@@ -147,24 +191,32 @@ func getQueue(ns *servicebus.Namespace, queueName string, receiveMode string) (*
 
 func (qrcvr *QueueReceiver) listen() {
 	//fmt.Println("Setting up listener...")
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-	//defer cancel()
+	//ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	ctx, cancel := context.WithCancel(context.Background())
+
 	qrcvr.listenctxCancelFunc = cancel
-	listenHandle, err := qrcvr.q.Receive(ctx, func(ctx context.Context, message *servicebus.Message) servicebus.DispositionAction {
-		// text := string(message.Data)
-		err2 := qrcvr.processMessage(message)
-		if err2 == nil {
-			return message.Complete()
-		}
-		return message.Abandon()
-	})
-	qrcvr.listenerHandler = listenHandle
-	//defer listenHandle.Close(context.Background())
+	queueSession := &servicebus.QueueSession{}
+	if qrcvr.sessionID != "" {
+		queueSession = qrcvr.q.NewSession(&qrcvr.sessionID)
+	} else {
+		queueSession = qrcvr.q.NewSession(nil)
+	}
+
+	ssh := &StepSessionHandler{}
+	ssh.valueType = qrcvr.valueType
+	ssh.handler = qrcvr.handler
+	ssh.queueName = qrcvr.queueName
+
+	qrcvr.stepSessionHandler = ssh
+
+	err := queueSession.ReceiveOne(ctx, ssh)
 
 	if err != nil {
 		log.Error(err.Error())
 		return
 	}
+
+	defer cancel()
 
 	//fmt.Println("I am listening...")
 	log.Infof("QueueReceiver is now polling on Queue [%s]", qrcvr.queueName)
@@ -177,14 +229,14 @@ func (qrcvr *QueueReceiver) listen() {
 
 }
 
-func (qrcvr *QueueReceiver) processMessage(msg *servicebus.Message) error {
+func (ssh *StepSessionHandler) processMessage(msg *servicebus.Message, handler *trigger.Handler, valueType string, queueName string) error {
 	// log.Infof("Processing record from Topic[%s], Partition[%d], Offset[%d]", msg.Topic, msg.Partition, msg.Offset)
 	var outputRoot = map[string]interface{}{}
 	var brokerProperties = map[string]interface{}{}
 	outputData := make(map[string]interface{})
 
 	if msg.Data != nil {
-		deserVal := qrcvr.valueType
+		deserVal := valueType
 		if deserVal == "String" {
 			text := string(msg.Data)
 			outputRoot["messageString"] = string(text)
@@ -210,12 +262,12 @@ func (qrcvr *QueueReceiver) processMessage(msg *servicebus.Message) error {
 		}
 	}
 
-	_, err := qrcvr.handler.Handle(context.Background(), outputData)
+	_, err := handler.Handle(context.Background(), outputData)
 	if err != nil {
-		log.Errorf("Failed to process record from Queue [%s], due to error - %s", qrcvr.queueName, err.Error())
+		log.Errorf("Failed to process record from Queue [%s], due to error - %s", queueName, err.Error())
 	} else {
 		// record is successfully processed.
-		log.Infof("Record from Queue [%s] is successfully processed", qrcvr.queueName)
+		log.Infof("Record from Queue [%s] is successfully processed", queueName)
 	}
 	return err
 }
@@ -230,7 +282,7 @@ func (t *SBQueueReceiverTrigger) Stop() error {
 		log.Debugf("About to close ListenerHandler for Queue [%s]", qrcvr.queueName)
 		select {
 		case <-time.After(2 * time.Second):
-			qrcvr.listenerHandler.Close(context.Background())
+			qrcvr.stepSessionHandler.messageSession.Close()
 		}
 		qrcvr.listenctxCancelFunc()
 	}
